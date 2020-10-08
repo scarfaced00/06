e751608470a121957cf01a50f07fb0a12c130a55
scarfaced00 <jazmyne@naver.com>
2020-10-08 16:39:27 +0900

    ex4

--- main.c
+++ main.c
@@ -3,15 +3,35 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int factorial(int n)
 {
-	return (a*a);
+	int i;
+	int result = 1;
+	for (i = 1; n >= i; n--)
+		result *= n;
+	return result;
+}
+
+int get_integer()
+{
+  int num;
+	printf("input an integer:");
+	scanf("%i", &num);
+
+  return num;
+}
+
+int combination(int n, int r)
+{
+	return factorial(n) / factorial(r) / factorial(n - r);
 }
 
 int main()
 {
-	int a = 2;
-	a=square(a);
-	printf("a=%i\n", a);
-}
+  int n,r,result;
+  n=get_integer();
+  r=get_integer();
+  result=combination(n,r);
 
+  printf("combination result is %i\n",result);
+}
